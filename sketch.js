fa023cd86c4cb3484ec406bf21cd426fac899bb1
Henderson Reed Hummel <reed.hummel@gmail.com>
2024-01-21 16:44:26 -0800

    many fixes

--- sketch.js
+++ sketch.js
@@ -4,34 +4,39 @@ let aAeolianScale = ['A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4',
 let aPhrygianScale = ['A2', 'Bb2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'Bb3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4'];
 let aMelodicMinorScale = ['A2', 'B2', 'C3', 'D3', 'E3', 'F#3', 'G#3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F#4', 'G#4', 'A4'];
 
+
+let brighter = [
+	[23, 230, 226],
+	[68, 207, 238],
+	[201, 235, 30],
+	[255, 39, 10],
+	[22, 216, 223],
+]
+
+
 let state;
 function setup() {
 	state = {
+		// config items:
+		mode: "development", // swap between "development" and "webcam"
+		step: 16, // how many times per minute we should check for notes.
+		scale: aNaturalMinorScale,
+		threshold: 185, // lower means more notes per second. 3.28 is a good semi-continuous song, with occasional moments of silence.
+		maxThreshold: 200,
+		noteLengths: ["4n", "2n"],
+		colors: brighter,
+		maxSynths: 3,
+		// dont change anything below this point.
+		scaleSelect: undefined, // only defined in development mode.
+		thresholdSlider: undefined, // only defined when in development mode.
 		media: undefined,
-		mode: "webcam",
 		points: [],
 		pointSynths: [],
 		synthIndex: 0,
 		audioRunning: false,
-		step: 8, // how many times per minute we should check for notes.
-		lastChecked: 0, // a variable used in combination with above `step`
-		threshold: 3.28, // lower means more notes per second. 3.28 is a good semi-continuous song, with occasional moments of silence.
-		thresholdSlider: undefined, // only defined when in development mode.
-		scale: aNaturalMinorScale,
-		scaleSelect: undefined, // only defined in development mode.
-		noteLengths: ["4n", "2n"],
-		colors: [
-			[21, 122, 110],
-			[30, 255, 188],
-			[214, 104, 83],
-			[122, 40, 203],
-			[165, 216, 255],
-		]
 	}
 
 	createCanvas(640, 480);
-
-	state.mode = "development"; // change to "webcam" to begin using webcam.
 
 	if (state.mode == "webcam") {
 		state.media = createCapture(VIDEO);
@@ -40,7 +45,7 @@ function setup() {
 	}
 	else if (state.mode == "development") {
 		// dev mode adds a slider for threshold and uses a video file, rather than the webcam.
-		state.thresholdSlider = createSlider(0, 10, state.threshold, 0);
+		state.thresholdSlider = createSlider(0, state.maxThreshold, state.threshold, 0);
 		state.scaleSelect = createSelect();
 
 		state.scaleSelect.option('aNaturalMinorScale');
@@ -58,34 +63,34 @@ function setup() {
 
 	// set imagemode for easy scaling and cropping.
 	imageMode(CENTER);
-}
-
-function mouseClicked() {
-	initializePoints();
-	state.media.loop();
-	state.media.volume(0);
-	state.audioRunning = true;
-	Tone.start();
+	Tone.context.blockSize = 1024;
 }
 
 function draw() {
 	background(255);
 	image(state.media, width / 1.5, height / 1.5, width * 1.5, height * 1.5);
-	state.scale = eval(state.scaleSelect.selected());
 	drawAndCheckPoints();
 }
 
+function mouseClicked() {
+	initializePoints();
+	if (state.mode == "development") {
+		state.media.loop();
+		state.media.volume(0);
+	}
+	state.audioRunning = true;
+	Tone.start();
+}
 
 function initializePoints() {
 	// initializing synthesizers
-	for (let i = 0; i < 9; i++) {
+	for (let i = 0; i < state.maxSynths; i++) {
 		let synth;
 		if (i > 0 && i < 3) {
 			synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
 		}
 		else if (i >= 3 && i < 6) {
 			synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
-			//synth = new Tone.AMSynth().toDestination();
 		}
 		else {
 			const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
@@ -111,7 +116,7 @@ function initializePoints() {
 					y: y,
 					colors: [],
 					pastColors: [],
-					color: "rgba(30, 255, 188, 0.55)",
+					pointColor: [30, 255, 188],
 				});
 		}
 	}
@@ -137,39 +142,47 @@ function euclideanDistance(colors, pastColors) {
 }
 
 function drawAndCheckPoints() {
-	//state.threshold = state.thresholdSlider.value(); // 7-9 is a good value, now that we're running just 8-16 times a second.
-	state.points.forEach((point) => {
-		point.pastColors = point.colors;
-		point.colors = get(point.x, point.y);
+	let now = Tone.now();
+	if (timeToRun(now)) {
+		state.points.forEach((point) => {
+			point.pastColors = point.colors;
+			point.colors = get(point.x, point.y);
 
-		// drawing the point must happen after sampling the video color (otherwise we just get the color of the point.)
-		drawPoint(point);
-
-		let now = Tone.now();
-		if (timeToRun(now)) { // should be around $step times every second.
 			checkDistanceAndTriggerNote(point, now);
-		}
+		});
+	}
+	// drawing the point must happen after sampling the video color (otherwise we just get the color of the point.)
+	state.points.forEach((point) => {
+		drawPoint(point);
 	});
 }
 
 function checkDistanceAndTriggerNote(point, now) {
 	if (state.mode == "development") {
 		state.threshold = state.thresholdSlider.value();
+		state.scale = eval(state.scaleSelect.selected());
 	}
-	if (euclideanDistance(point.colors, point.pastColors) > state.threshold && state.audioRunning) {
+	let dist = euclideanDistance(point.colors, point.pastColors);
+	if (dist > state.threshold && state.audioRunning) {
 		// TODO: could select note/notelength using an equation with an uneven distribution? to emphasize root notes, etc.
 		const randomScaleIndex = Math.floor(Math.random() * scale.length);
-		const noteLengthIndex = Math.floor(Math.random() * noteLengths.length);
+		const noteLengthIndex = Math.floor(Math.random() * state.noteLengths.length);
 
 		triggerNote(now, state.scale[randomScaleIndex], state.noteLengths[noteLengthIndex]);
 		setNewColor(point);
 	}
 }
 
+function setNewColor(point) {
+	point.pointColor = state.colors[Math.floor(Math.random() * state.colors.length)];
+}
+
 // TODO: could change the color values over time.
 function drawPoint(point) {
-	fill(color(point.color + [0.55]));
-	stroke(color(point.color + [1.0]));
+	let fillColor = color([...point.pointColor, 100]);
+	let strokeColor = color([...point.pointColor, 255]);
+	fill(fillColor);
+	stroke(strokeColor);
 	strokeWeight(2);
 	ellipse(point.x, point.y, 30, 30);
 }
@@ -182,17 +195,18 @@ function triggerNote(time, note, noteLength) {
 			noteLength,
 			time);
 		state.synthIndex++;
-		if (state.synthIndex > 8) {
+		if (state.synthIndex >= state.maxSynths) {
 			state.synthIndex = 0;
 		}
 	}
 }
 
 // turns true $state.step times per second.
+let lastChecked = 0; // a variable used in combination with above `step`
 function timeToRun(time) {
 	const checkInterval = 1 / state.step;
-	if ((time - state.lastChecked) >= checkInterval) {
-		state.lastChecked = time;
+	if ((time - lastChecked) >= checkInterval) {
+		lastChecked = time;
 		return true;
 	}
 	else { return false; }
