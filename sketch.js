96fa6b9879672ce9a236664dba01e6ab0b997176
Henderson Reed Hummel <reed.hummel@gmail.com>
2024-01-26 12:27:27 -0800

    implemented drag-n-drop of content.

--- sketch.js
+++ sketch.js
@@ -13,13 +13,18 @@ let brighter = [
 	[22, 216, 223],
 ]
 
+function videoLoaded() {
+	//state.canvas = createCanvas(state.media.width, state.media.height);
+	state.canvas = createCanvas(640, 480);
+}
+
 
 let state;
 function setup() {
 	state = {
 		// config items:
-		mode: "development", // swap between "development" and "webcam"
-		step: 16, // how many times per minute we should check for notes.
+		mode: "webcam", // swap between "development" and "webcam"
+		step: 8, // how many times per minute we should check for notes.
 		scale: aNaturalMinorScale,
 		threshold: 185, // lower means more notes per second. 3.28 is a good semi-continuous song, with occasional moments of silence.
 		maxThreshold: 200,
@@ -27,21 +32,23 @@ function setup() {
 		colors: brighter,
 		maxSynths: 3,
 		// dont change anything below this point.
+		canvas: undefined,
 		scaleSelect: undefined, // only defined in development mode.
 		thresholdSlider: undefined, // only defined when in development mode.
 		media: undefined,
 		points: [],
-		pointSynths: [],
 		synthIndex: 0,
 		audioRunning: false,
+		imgX: 0,
+		imgY: 0,
+		imgScale: 1.0,
 	}
 
-	createCanvas(640, 480);
 
 	if (state.mode == "webcam") {
-		state.media = createCapture(VIDEO);
-		state.media.size(640, 480);
+		state.media = createCapture(VIDEO, videoLoaded);
 		state.media.hide();
+		//state.canvas = createCanvas(state.media.width, state.media.height);
 	}
 	else if (state.mode == "development") {
 		// dev mode adds a slider for threshold and uses a video file, rather than the webcam.
@@ -54,21 +61,49 @@ function setup() {
 		state.scaleSelect.option('aPhrygianScale');
 		state.scaleSelect.option('aMelodicMinorScale');
 
-		state.media = createVideo('output.mp4');
+		state.media = createVideo('output.mp4', videoLoaded);
 		state.media.hide();
 	}
 	else {
 		console.log("mode not supported.")
 	}
 
-	// set imagemode for easy scaling and cropping.
-	imageMode(CENTER);
+
 	Tone.context.blockSize = 1024;
+}
+
+function displayMedia(img) {
+	translate(width / 2, height / 2);
+	scale(state.imgScale);
+	translate(-img.width / 2 - state.imgX, -img.height / 2 - state.imgY);
+	image(img, 0, 0);
+}
+
+function mouseDragged() {
+	// Dragging logic, to allow picking/placing media stream
+	if (mouseIsPressed) {
+		state.imgX += pmouseX - mouseX;
+		state.imgY += pmouseY - mouseY;
+	}
+}
+
+function mouseWheel(event) {
+	// Zoom logic with scroll wheel
+	let delta = event.delta;
+	state.imgScale += delta * 0.001; // Adjust the zoom speed as needed
+
+	// Constrain the zoom level if necessary
+	state.imgScale = constrain(state.imgScale, 0.5, 5);
+
+	// Prevent the default behavior of scrolling (e.g., page scrolling)
+	return false;
 }
 
 function draw() {
 	background(255);
-	image(state.media, width / 1.5, height / 1.5, width * 1.5, height * 1.5);
+	displayMedia(state.media);
+	//filter(BLUR);
+	filter(POSTERIZE, 16);
 	drawAndCheckPoints();
 }
 
@@ -82,44 +117,38 @@ function mouseClicked() {
 	Tone.start();
 }
 
+function generateCircleCoordinates(N, radius) {
+	let coordinates = [];
+	for (let i = 0; i < N; i++) {
+		const angle = (i / N) * TWO_PI; // Calculate the angle based on the number of points
+		const x = floor(radius * cos(angle));  // Calculate x-coordinate
+		const y = floor(radius * sin(angle));  // Calculate y-coordinate
+		coordinates.push({ x, y });
+	}
+	return coordinates;
+}
+
 function initializePoints() {
-	// initializing synthesizers
-	for (let i = 0; i < state.maxSynths; i++) {
-		let synth;
-		if (i > 0 && i < 3) {
-			synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
-		}
-		else if (i >= 3 && i < 6) {
-			synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
-		}
-		else {
-			const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
-			synth = new Tone.PolySynth().toDestination();
-		}
-		state.pointSynths.push({ 'synth': synth, 'ts': 0 });
-	}
-	// creating point data
-	let cols = 3;
-	let rows = 3;
-	let spacingX = width / (cols + 1);
-	let spacingY = height / (rows + 1);
-
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			let x = (i + 1) * spacingX;
-			let y = (j + 1) * spacingY;
-
-			//points
-			state.points.push(
-				{
-					x: x,
-					y: y,
-					colors: [],
-					pastColors: [],
-					pointColor: [30, 255, 188],
-				});
-		}
-	}
+	let circCoords = generateCircleCoordinates(state.maxSynths, 60)
+	circCoords.forEach((coord) => {
+		//const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination();
+		const phaser = new Tone.Phaser({
+			frequency: 3,
+			octaves: 3,
+			baseFrequency: 1000
+		}).toDestination();
+		const synth = new Tone.PolySynth().connect(phaser);
+		state.points.push(
+			{
+				x: coord.x + width / 2,
+				y: coord.y + height / 2,
+				colors: [],
+				pastColors: [],
+				pointColor: [30, 255, 188],
+				synth: synth,
+				ts: 0,
+			});
+	});
 }
 
 function euclideanDistance(colors, pastColors) {
@@ -188,9 +217,9 @@ function drawPoint(point) {
 }
 
 function triggerNote(time, note, noteLength) {
-	if (state.pointSynths[state.synthIndex].ts != time) {
-		state.pointSynths[state.synthIndex].ts = time
-		state.pointSynths[state.synthIndex].synth.triggerAttackRelease(
+	if (state.points[state.synthIndex].ts < time) {
+		state.points[state.synthIndex].ts = time
+		state.points[state.synthIndex].synth.triggerAttackRelease(
 			note,
 			noteLength,
 			time);
@@ -207,6 +236,7 @@ function timeToRun(time) {
 	const checkInterval = 1 / state.step;
 	if ((time - lastChecked) >= checkInterval) {
 		lastChecked = time;
+		console.log("timeToRun");
 		return true;
 	}
 	else { return false; }
